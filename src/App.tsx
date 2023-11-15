e084a0f7acee172b66ff18d196aed20e4d948044
Joma Work <johanmfc11@gmail.com>
2023-11-15 13:02:26 -0400

    Add todo functionality to Todo App

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,150 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React, { useState } from 'react';
+import './App.css';
 
-function App() {
-  const [count, setCount] = useState(0)
+const data: Todo[] = [
+  {
+    id: '123-123-123-123-123',
+    title: 'Learn React',
+    completed: false,
+  },
+  {
+    id: '222-222-222-222-222',
+    title: 'Learn TypeScript',
+    completed: false,
+  },
+  {
+    id: '333-333-333-333-333',
+    title: 'Learn Redux',
+    completed: false,
+  },
+];
 
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+type IdTodo = `${string}-${string}-${string}-${string}-${string}`;
+
+interface Todo {
+  id: IdTodo;
+  title: string;
+  completed: boolean;
 }
 
-export default App
+const Item = ({
+  todo,
+  deleteTodo,
+  editTodo,
+}: {
+  todo: Todo;
+  deleteTodo: (id: IdTodo) => void;
+  editTodo: (id: IdTodo, title: string) => void;
+}) => {
+  const [isEditing, setIsEditing] = useState(false);
+  const [newTitle, setNewTitle] = useState(todo.title);
+
+  return (
+    <li key={todo.id}>
+      {isEditing ? (
+        <>
+          <input
+            type='text'
+            value={newTitle}
+            onChange={e => setNewTitle(e.target.value)}
+          />
+          <button
+            onClick={() => {
+              editTodo(todo.id, newTitle);
+              setIsEditing(false);
+            }}
+          >
+            Save
+          </button>
+          <button onClick={() => setIsEditing(!isEditing)}>
+            <span role='img' aria-label='edit'>
+              ✏️
+            </span>
+          </button>
+        </>
+      ) : (
+        <>
+          {todo.title}
+          <button onClick={() => deleteTodo(todo.id)}>❌</button>
+          <button onClick={() => setIsEditing(!isEditing)}>
+            <span role='img' aria-label='edit'>
+              ✏️
+            </span>
+          </button>
+        </>
+      )}
+    </li>
+  );
+};
+
+function App() {
+  const [todos, setTodos] = useState(data);
+  const [item, setItem] = useState('');
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setItem(e.target.value);
+  };
+
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    if (!item) return;
+
+    const newItem = {
+      id: crypto.randomUUID(),
+      title: item,
+      completed: false,
+    };
+
+    setTodos([...todos, newItem]);
+    setItem('');
+  };
+
+  const deleteTodo = (id: IdTodo): void => {
+    const updatedTodos = todos.filter(todo => todo.id !== id);
+    setTodos(updatedTodos);
+  };
+
+  const editTodo = (id: IdTodo, title: string): void => {
+    const updatedTodos = todos.map(todo => {
+      if (todo.id === id) {
+        return {
+          ...todo,
+          title,
+        };
+      }
+      return todo;
+    });
+    setTodos(updatedTodos);
+  };
+
+  return (
+    <main>
+      <h1>Todo App</h1>
+
+      <form onSubmit={handleSubmit}>
+        <input
+          type='text'
+          placeholder='Add todo'
+          value={item}
+          onChange={handleChange}
+        />
+        <button type='submit' disabled={!item}>
+          Add
+        </button>
+      </form>
+
+      <ul>
+        {todos.map(todo => (
+          <Item
+            key={todo.id}
+            todo={todo}
+            deleteTodo={deleteTodo}
+            editTodo={editTodo}
+          />
+        ))}
+      </ul>
+    </main>
+  );
+}
+
+export default App;
